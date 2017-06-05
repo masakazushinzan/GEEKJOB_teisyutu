d35c34ee22be6c37d030b5b9c8aec1a09fe8d035
DESKTOP-QB7O006\rui <zenryokutiuhi24@gmail.com>
2017-06-05 11:11:46 +0900

    現在の日時の表示

---
+++ CampChallenge_java/015.標準クラス/Time_stamp2.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kadai;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.Calendar;
+
+/**
+ *
+ * @author rui
+ */
+public class Time_stamp2 extends HttpServlet {
+Date d = new Date();
+    Calendar c = Calendar.getInstance();
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+           Date time = new Date();
+           int year = c.get(Calendar.YEAR);
+           int month = c.get(Calendar.MONTH)+1;
+           int day = c.get(Calendar.DATE);
+           int hour = c.get(Calendar.HOUR_OF_DAY);
+           int minute = c.get(Calendar.MINUTE);
+           int second = c.get(Calendar.SECOND);
+
+          
+//             c.getTime();
+//             out.print(c.YEAR+"年"+c.MONTH+"月");
+             out.print(year+"年"+month+"月"+day+"日"+hour+"時"+minute+"分"
+             +second+"秒");
+
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
