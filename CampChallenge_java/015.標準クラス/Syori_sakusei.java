21aeeb4f17ca8cb5c21306bbaf9a049b758aad51
DESKTOP-QB7O006\rui <zenryokutiuhi24@gmail.com>
2017-06-06 11:36:34 +0900

    標準クラスの利用

---
+++ CampChallenge_java/015.標準クラス/Syori_sakusei.java
@@ -0,0 +1,95 @@
+/*
+     * To change this license header, choose License Headers in Project Properties.
+     * To change this template file, choose Tools | Templates
+     * and open the template in the editor.
+ */
+package kadai_syori;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.Date;
+
+/**
+ *
+ * @author rui
+ */
+public class Syori_sakusei extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            int i = 0;
+            Date dat = new Date();
+            File fp = new File("log.txt");
+            FileReader fr = new FileReader(fp);
+            BufferedReader bf = new BufferedReader(fr);
+            FileWriter fw = new FileWriter(fp);
+            fw.write(dat + "処理開始<br>");
+            i = (int)Math.pow(5,5);
+            
+            
+            
+            out.print("(int)Math.pow(5,5)=５の５乗は" +
+""+i+"<br><br>");
+            fw.write(dat + "処理終了");
+            fw.close();
+            out.print(bf.readLine());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
